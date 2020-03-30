2bec666873e13f06a3ff1f7bac60afd566b2fe43
henrikengelbrink <henrik@keeet.io>
2020-03-30 12:44:22 +0200

    feat: Add exception handler

--- src/main/kotlin/se09/user/service/exceptions/APIExceptionCode.kt
+++ src/main/kotlin/se09/user/service/exceptions/APIExceptionCode.kt
@@ -4,7 +4,6 @@ import io.micronaut.http.HttpStatus
 
 enum class APIExceptionCode(val httpCode: HttpStatus) {
     USER_ALREADY_EXISTS(HttpStatus.CONFLICT),
-    INVALID_PASSWORD_FORMAT(HttpStatus.BAD_REQUEST),
     LOGIN_INVALID(HttpStatus.UNAUTHORIZED),
     UNKNOWN_USER(HttpStatus.NOT_FOUND)
 }
--- src/main/kotlin/se09/user/service/exceptions/APIExceptionHandler.kt
+++ src/main/kotlin/se09/user/service/exceptions/APIExceptionHandler.kt
@@ -4,6 +4,7 @@ import io.micronaut.context.annotation.Requires;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Produces;
+import io.micronaut.http.client.exceptions.HttpClientResponseException
 import io.micronaut.http.server.exceptions.ExceptionHandler;
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -21,6 +22,21 @@ class APIExceptionHandler : ExceptionHandler<APIException, HttpResponse<Any>> {
     override fun handle(request: HttpRequest<Any>, exception: APIException): HttpResponse<Any> {
         LOG.warn("########### APIException ${exception.code.name} ${exception.code.httpCode.code}")
         return HttpResponse.status(exception.code.httpCode)
+    }
+
+}
+
+@Produces
+@Singleton
+@Requires(classes = [HttpClientResponseException::class, ExceptionHandler::class])
+class HttpClientResponseExceptionHandler : ExceptionHandler<HttpClientResponseException, HttpResponse<Any>> {
+
+    private val LOG: Logger = LoggerFactory.getLogger(APIExceptionHandler::class.java)
+
+    @Override
+    override fun handle(request: HttpRequest<Any>, exception: HttpClientResponseException): HttpResponse<Any> {
+        LOG.warn("########### HttpClientResponseException ${exception.status}")
+        return HttpResponse.status(exception.status)
     }
 
 }
